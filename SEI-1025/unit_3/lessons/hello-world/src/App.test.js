da250976dc33ec727950469a8006e3cfd32d760b
Mikalai19 <bertman29@gmail.com>
2021-12-16 13:50:39 -0800

    test

--- SEI-1025/unit_3/lessons/hello-world/src/App.test.js
+++ SEI-1025/unit_3/lessons/hello-world/src/App.test.js
@@ -1,8 +1,55 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Forum from './Forum';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+test('renders dashboard tag', () => {
+  render(<Forum />);
+  const spanElement = screen.getByText(/Dashboard/i);
+  expect(spanElement).toBeInTheDocument();
+});
+
+test('renders customers tag', () => {
+  render(<Forum />);
+  const spanElement = screen.getByText(/Customers/i);
+  expect(spanElement).toBeInTheDocument();
+});
+
+test('render tag called Payments', () => {
+  render(<Forum />);
+  const spanElement = screen.getByText(/Payments/i);
+  expect(spanElement).toBeInTheDocument();
+});
+
+test('render tag called Authentication', () => {
+  render(<Forum />);
+  const spanElement = screen.getByText(/Payments/i);
+  expect(spanElement).toBeInTheDocument();
+});
+
+test('render tag called Payments', () => {
+  render(<Forum />);
+  const spanElement = screen.getByText(/Authentication/i);
+  expect(spanElement).toBeInTheDocument();
+});
+
+
+test('Transfers check', () => {
+  render(<Forum />);
+  const spanElement = screen.getByText(/Transfers/i);
+  expect(spanElement).toBeInTheDocument();
+});
+
+
+test('Balance check', () => {
+  render(<Forum />);
+  const spanElement = screen.getByText(/Balance/i);
+  expect(spanElement).toBeInTheDocument();
+});
+
+
+test('check if Question presents ', () => {
+  render(<Forum />);
+  const spanElement = screen.getByTitle(/Question/i);
+  expect(spanElement).toBeInTheDocument();
 });
